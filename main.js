0e0f9e537aaa569f215bd39d714c62b6d433c8e5
Adam Babs <43379013+adambabs@users.noreply.github.com>
2021-12-17 16:49:33 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let margin = {
     top: 0,
     right: 0,
     bottom: 0,
-    left: 0
+    left: 40
   },
   width = 1450 - margin.left - margin.right,
   height = 900 - margin.top - margin.bottom;
